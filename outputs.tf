ea968b0b59a50aa85342a4e882d6a23fd98bef32
Jeremiah Norris <jjno91@gmail.com>
2019-02-01 17:38:17 -0600

    add worker config map and kubeconfig outputs

--- outputs.tf
+++ outputs.tf
@@ -22,3 +22,64 @@ output "control_sg_id" {
   description = "Security group ID for the cluster's control plane"
   value       = "${aws_security_group.control.id}"
 }
+
+locals {
+  worker_config_map = <<EOF
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: aws-auth
+  namespace: kube-system
+data:
+  mapRoles: |
+    - rolearn: ${aws_iam_role.eks_worker.arn}
+      username: system:node:{{EC2PrivateDNSName}}
+      groups:
+        - system:bootstrappers
+        - system:nodes
+EOF
+}
+
+output "worker_config_map" {
+  description = "Kubernetes config map that must be loaded to the EKS cluster before worker nodes can connect"
+  value       = "${local.worker_config_map}"
+}
+
+locals {
+  kubeconfig = <<EOF
+apiVersion: v1
+clusters:
+- cluster:
+    server: ${aws_eks_cluster.this.endpoint}
+    certificate-authority-data: ${aws_eks_cluster.this.certificate_authority.0.data}
+  name: kubernetes
+contexts:
+- context:
+    cluster: kubernetes
+    user: aws
+  name: aws
+current-context: aws
+kind: Config
+preferences: {}
+users:
+- name: aws
+  user:
+    exec:
+      apiVersion: client.authentication.k8s.io/v1alpha1
+      command: aws-iam-authenticator
+      args:
+        - "token"
+        - "-i"
+        - "${aws_eks_cluster.this.id}"
+        # - "-r"
+        # - "<role-arn>"
+      # env:
+        # - name: AWS_PROFILE
+        #   value: "<aws-profile>"
+EOF
+}
+
+output "kubeconfig" {
+  description = "https://kubernetes.io/docs/concepts/configuration/organize-cluster-access-kubeconfig/"
+  value       = "${local.kubeconfig}"
+}
