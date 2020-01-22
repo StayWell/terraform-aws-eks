404811ed5ce39108e1936a2922f421b187ca59bf
Jeremiah Norris <jjno91@gmail.com>
2020-01-22 15:41:21 -0600

    convert to node groups

--- main.tf
+++ main.tf
@@ -1,260 +1,92 @@
-#################################################
-# Control Plane
-#################################################
-
 resource "aws_eks_cluster" "this" {
-  name     = "${var.env}"
-  role_arn = "${aws_iam_role.control.arn}"
-  version  = "${var.kubernetes_version}"
+  name     = var.id
+  role_arn = aws_iam_role.cluster.arn
+  version  = var.kubernetes_version
 
   vpc_config {
-    subnet_ids         = ["${var.private_subnet_ids}", "${var.public_subnet_ids}"]
-    security_group_ids = ["${aws_security_group.control.id}"]
-  }
-}
-
-# IAM ###########################################
-
-data "aws_iam_policy_document" "control" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["eks.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_role" "control" {
-  name_prefix        = "${var.env}-eks-control-"
-  assume_role_policy = "${data.aws_iam_policy_document.control.json}"
-  tags               = "${merge(map("Name", "${var.env}-eks-control"), var.tags)}"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_iam_role_policy_attachment" "cluster" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = "${aws_iam_role.control.name}"
-}
-
-resource "aws_iam_role_policy_attachment" "service" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-  role       = "${aws_iam_role.control.name}"
-}
-
-# Security Group ################################
-
-resource "aws_security_group" "control" {
-  name_prefix = "${var.env}-eks-control-"
-  vpc_id      = "${var.vpc_id}"
-  tags        = "${merge(map("Name", "${var.env}-eks-control"), var.tags)}"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_security_group_rule" "control_ingress_worker" {
-  description              = "EKS worker nodes"
-  type                     = "ingress"
-  from_port                = 443
-  to_port                  = 443
-  protocol                 = "tcp"
-  security_group_id        = "${aws_security_group.control.id}"
-  source_security_group_id = "${aws_security_group.worker.id}"
-}
-
-resource "aws_security_group_rule" "control_egress_worker" {
-  description              = "EKS worker nodes"
-  type                     = "egress"
-  from_port                = 0
-  to_port                  = 0
-  protocol                 = "-1"
-  security_group_id        = "${aws_security_group.control.id}"
-  source_security_group_id = "${aws_security_group.worker.id}"
-}
-
-#################################################
-# Workers
-#################################################
-
-data "aws_ami" "linux" {
-  owners      = ["${var.trusted_ami_account}"]
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["${var.linux_ami_name}"]
-  }
-}
-
-data "aws_region" "this" {}
-
-locals {
-  linux_userdata = <<EOF
-#!/bin/bash
-set -o xtrace
-source /etc/eks/bootstrap.sh ${aws_eks_cluster.this.name}
-EOF
-}
-
-resource "aws_launch_template" "linux" {
-  name_prefix            = "${var.env}-eks-linux-"
-  image_id               = "${data.aws_ami.linux.image_id}"
-  instance_type          = "${var.linux_instance_type}"
-  ebs_optimized          = true
-  user_data              = "${base64encode(local.linux_userdata)}"
-  vpc_security_group_ids = ["${aws_security_group.worker.id}"]
-  tags                   = "${merge(map("Name", "${var.env}-eks-linux"), var.tags)}"
-
-  block_device_mappings {
-    device_name = "${data.aws_ami.linux.root_device_name}"
-
-    ebs {
-      volume_size = "${var.linux_disk_size}"
-    }
+    subnet_ids = ["${var.private_subnet_ids}", "${var.public_subnet_ids}"]
   }
 
-  iam_instance_profile {
-    name = "${aws_iam_instance_profile.this.name}"
-  }
-
-  monitoring {
-    enabled = true
-  }
-
-  tag_specifications {
-    resource_type = "volume"
-    tags          = "${merge(map("Name", "${var.env}-eks-linux"), var.tags)}"
-  }
-
-  tag_specifications {
-    resource_type = "instance"
-    tags          = "${merge(map("Name", "${var.env}-eks-linux", "kubernetes.io/cluster/${aws_eks_cluster.this.id}", "owned"), var.tags)}"
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_autoscaling_group" "linux" {
-  name_prefix         = "${var.env}-eks-linux-"
-  min_size            = "${var.linux_node_count}"
-  max_size            = "${var.linux_node_count}"
-  vpc_zone_identifier = ["${var.private_subnet_ids}"]
-  target_group_arns   = ["${var.linux_target_group_arns}"]
-
-  launch_template = {
-    id      = "${aws_launch_template.linux.id}"
-    version = "$$Latest"
-  }
-
-  enabled_metrics = [
-    "GroupMinSize",
-    "GroupMaxSize",
-    "GroupDesiredCapacity",
-    "GroupInServiceInstances",
-    "GroupPendingInstances",
-    "GroupStandbyInstances",
-    "GroupTerminatingInstances",
-    "GroupTotalInstances",
+  depends_on = [
+    "aws_iam_role_policy_attachment.AmazonEKSClusterPolicy",
+    "aws_iam_role_policy_attachment.AmazonEKSServicePolicy",
   ]
-
-  lifecycle {
-    create_before_destroy = true
-  }
 }
 
-# IAM ###########################################
+resource "aws_iam_role" "cluster" {
+  name = var.id
 
-data "aws_iam_policy_document" "worker" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
     }
-  }
+  ]
+}
+POLICY
 }
 
-resource "aws_iam_role" "worker" {
-  name_prefix        = "${var.env}-eks-worker-"
-  assume_role_policy = "${data.aws_iam_policy_document.worker.json}"
-  tags               = "${merge(map("Name", "${var.env}-eks-worker"), var.tags)}"
-
-  lifecycle {
-    create_before_destroy = true
-  }
+resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.cluster.name
 }
 
-resource "aws_iam_role_policy_attachment" "node" {
+resource "aws_iam_role_policy_attachment" "AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.cluster.name
+}
+
+resource "aws_eks_node_group" "this" {
+  cluster_name    = aws_eks_cluster.this.name
+  node_group_name = var.id
+  node_role_arn   = aws_iam_role.node.arn
+  subnet_ids      = var.private_subnet_ids
+
+  scaling_config {
+    desired_size = var.desired_size
+    max_size     = var.max_size
+    min_size     = var.min_size
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
+
+resource "aws_iam_role" "node" {
+  name = var.id
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = "${aws_iam_role.worker.name}"
+  role       = aws_iam_role.node.name
 }
 
-resource "aws_iam_role_policy_attachment" "cni" {
+resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = "${aws_iam_role.worker.name}"
+  role       = aws_iam_role.node.name
 }
 
-resource "aws_iam_role_policy_attachment" "ecr" {
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = "${aws_iam_role.worker.name}"
-}
-
-resource "aws_iam_instance_profile" "this" {
-  name_prefix = "${var.env}-"
-  role        = "${aws_iam_role.worker.name}"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-# Security Group ################################
-
-resource "aws_security_group" "worker" {
-  name_prefix = "${var.env}-eks-worker-"
-  vpc_id      = "${var.vpc_id}"
-  tags        = "${merge(map("Name", "${var.env}-eks-worker"), var.tags)}"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_security_group_rule" "worker_ingress_self" {
-  description       = "self"
-  type              = "ingress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  security_group_id = "${aws_security_group.worker.id}"
-  self              = true
-}
-
-resource "aws_security_group_rule" "worker_ingress_control" {
-  description              = "EKS control plane"
-  type                     = "ingress"
-  from_port                = 0
-  to_port                  = 0
-  protocol                 = "-1"
-  security_group_id        = "${aws_security_group.worker.id}"
-  source_security_group_id = "${aws_security_group.control.id}"
-}
-
-resource "aws_security_group_rule" "worker_egress_all" {
-  description       = "internet"
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  security_group_id = "${aws_security_group.worker.id}"
-  cidr_blocks       = ["0.0.0.0/0"]
+  role       = aws_iam_role.node.name
 }
--- variables.tf
+++ variables.tf
@@ -1,59 +1,44 @@
-variable "vpc_id" {
-  description = "(required) ID of the VPC to which the EKS cluster will be deployed"
-  default     = ""
-}
-
 variable "private_subnet_ids" {
-  description = "(required) IDs of the subnets to which the EKS worker nodes will be deployed"
+  description = "(Required) IDs of the subnets to which the EKS worker nodes will be deployed"
   default     = []
 }
 
 variable "public_subnet_ids" {
-  description = "(required) IDs of the subnets to which the EKS load balancers will be deployed"
+  description = "(Required) IDs of the subnets to which the EKS load balancers will be deployed"
   default     = []
 }
 
-variable "env" {
-  description = "(optional) Unique identifier used to name all resources"
+variable "id" {
+  description = "(Optional) Unique identifier used to name all resources"
   default     = "default"
 }
 
 variable "tags" {
-  description = "(optional) Additional tags applied to all resources"
+  description = "(Optional) Additional tags applied to all resources"
   default     = {}
 }
 
 variable "kubernetes_version" {
-  description = "(optional) https://www.terraform.io/docs/providers/aws/r/eks_cluster.html#version"
+  description = "(Optional) https://www.terraform.io/docs/providers/aws/r/eks_cluster.html#version"
   default     = ""
 }
 
 variable "trusted_ami_account" {
-  description = "(optional) ID of the account you trust AMIs from - defaulted to Amazon owned service account"
+  description = "(Optional) ID of the account you trust AMIs from - defaulted to Amazon owned service account"
   default     = "602401143452"
 }
 
-variable "linux_instance_type" {
-  description = "(optional) Linux worker node instance type"
-  default     = "m5.large"
+variable "desired_size" {
+  description = "(Optional) https://www.terraform.io/docs/providers/aws/r/eks_node_group.html#desired_size"
+  default     = "1"
 }
 
-variable "linux_ami_name" {
-  description = "(optional) String used to search for linux worker node AMI"
-  default     = "amazon-eks-node*"
+variable "max_size" {
+  description = "(Optional) https://www.terraform.io/docs/providers/aws/r/eks_node_group.html#max_size"
+  default     = "1"
 }
 
-variable "linux_disk_size" {
-  description = "(optional) Size of the root volume for linux worker nodes"
-  default     = "20"
-}
-
-variable "linux_node_count" {
-  description = "(optional) Static amount of linux worker nodes"
-  default     = "2"
-}
-
-variable "linux_target_group_arns" {
-  description = "(optional) https://www.terraform.io/docs/providers/aws/r/autoscaling_group.html#target_group_arns"
-  default     = []
+variable "min_size" {
+  description = "(Optional) https://www.terraform.io/docs/providers/aws/r/eks_node_group.html#min_size"
+  default     = "1"
 }
