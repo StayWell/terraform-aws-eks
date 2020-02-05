566e2dc42e4933ad803819c2d55525cdceb8de3e
Jeremiah Norris <jjno91@gmail.com>
2020-02-05 10:03:16 -0600

    add node_role_arn output

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,19 @@
 output "cluster_id" {
   description = "https://www.terraform.io/docs/providers/aws/r/eks_cluster.html#id"
-  value       = "${aws_eks_cluster.this.id}"
+  value       = aws_eks_cluster.this.id
 }
 
 output "cluster_endpoint" {
   description = "https://www.terraform.io/docs/providers/aws/r/eks_cluster.html#endpoint"
-  value       = "${aws_eks_cluster.this.endpoint}"
+  value       = aws_eks_cluster.this.endpoint
 }
 
 output "cluster_ca_data" {
   description = "https://www.terraform.io/docs/providers/aws/r/eks_cluster.html#data"
-  value       = "${aws_eks_cluster.this.certificate_authority.0.data}"
+  value       = aws_eks_cluster.this.certificate_authority.0.data
+}
+
+output "node_role_arn" {
+  description = "https://www.terraform.io/docs/providers/aws/r/eks_node_group.html#node_role_arn"
+  value       = aws_iam_role.node.arn
 }
